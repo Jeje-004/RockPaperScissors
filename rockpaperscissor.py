6ada9dbb4bf7b9ed257d08c01c5a88f0c0e27892
Jeje-004 <jenuje004@gmail.com>
2026-05-09 02:31:28 +0400

    Create rockpaperscissor.py

---
+++ rockpaperscissor.py
@@ -0,0 +1,56 @@
+Import random
+Bb[]
+print(“welcome to the Rock, Paper, Scissors game: “)
+
+rules=（””” ******RULES*******
+1) u choose and the comuter chooses
+2) Rock smashes Scissers-› Rock wins
+3) Scissers cuT Paper -> Scissers wins
+4) Paper covers Rock -› Paper wins   “””)
+
+Bo=input(“press Enter to contine or type (Help) for the rules “).capitalize()
+if Bo==“Help” :
+ print (rules)
+else :
+ print (“ok!”)
+
+chosses=(“rock ,paper ,scissers”)
+
+scissors= “✂️”
+paper= “📃”
+rock= “🪨”
+
+SO=input (“Enter ur choice (rock,paper, scissers): *). lower()
+If SO in chooses:
+ Print (“here we go”)
+  If So== “scissors” :
+    Print (f"u choose is scissors “,{scissors})
+  elif SO==“paper” :
+    Print (f"u choose is paper  “, {paper})
+  elif SO==“rock” :
+    Print (f"u choose is rock “, {rock})
+   else SO=“” :
+      Print (“no , choose again “)
+else :
+ Print (“not from chooses ,do it again! ”)
+
+
+Pcchice=rendom.choice([rock ,paper ,scissors])
+  If So== “scissors” :
+    Print (f"u choose is scissors “,{scissors})
+  elif SO==“paper” :
+    Print (f"u choose is paper  “, {paper})
+  elif SO==“rock” :
+    Print (f"u choose is rock “, {rock})
+   else SO=“” :
+      Print (“no , choose again “)
+
+Hows_win=(f “{Pcchice} , {SO}”)💔
+If :
+Print(“
+else:
+ Print (“
+   If:
+     Print 
+   elif :
+      Print (“
